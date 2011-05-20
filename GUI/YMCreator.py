5c88acc227f312202c3c66d100b3008671996521
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2011-05-20 18:40:25 -0300

    GUI enhanced

--- GUI/YMCreator.py
+++ GUI/YMCreator.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 {'configured': False}
 
 """Module for circuit creation"""
@@ -5,45 +6,111 @@
 import sys
 from PyQt4 import QtGui, QtCore
 
+class GetOpts(QtGui.QWidget):
+  
+    def __init__(self, track_type):
+        super(GetOpts, self).__init__()
+        
+        self.initUI(track_type)
+        
+    def initUI(self, track_type):
+	layout = self.make_all(self, track_type)
+	self.setLayout(layout)
 
-class TrackCreator():
-	def __init__(self, parent):
-		self.track_list = self.make_track_list(parent)
-		self.main_container = QtGui.QGroupBox('Escolha o tipo de trecho:')
-		self.main_layout = QtGui.QHBoxLayout()
-		self.options = QtGui.QGroupBox("Opcoes:", parent)
-		self.main_container.setLayout(self.main_layout)
+        self.resize(350, 300)
+
+    def make_all(self, parent, track_type):
+	if track_type == 'Escolha o tipo de pedaço: ':
+		layout = self.make_default(self)
+	elif track_type == 'Reta':
+		layout = self.make_straight(self)
+	elif track_type == 'Curva':
+		layout = self.make_curve(self)
+	else:
+		layout = self.make_clothoid(self)
+
+	return layout 
 	
-	def make_track_list(self, parent):
-		track_type = QtGui.QComboBox(parent)
-		track_type.addItem("Reta")
-		track_type.addItem("Curva")
-		track_type.addItem("Clotoide")
-		parent.connect(track_type, QtCore.SIGNAL('activated(QString)'), self.on_track_chosen)
-		return track_type
 
-	def on_track_chosen(self, text):
-		if text == 'Reta':
-			self.options = QtGui.QHBoxLayout(self,track_type)
+    def make_default(self, parent):
+	return QtGui.QVBoxLayout()
 
-			length_edit = QtGui.QLineEdit(parent = MainWindow)
-			length_edit.setText("Digite o comprimento da reta.")
-			
-			gerate_button = QtGui.QPushButton('Gerar Trecho', None)
-			
-			self.options_layout.addWidget(length_edit)
-			self.options_layout_.addWidget(generate_button)
+    def make_straight(self, parent):
+        length = QtGui.QLabel('Comprimento: ', parent)
 
-			#doreta
-			pass
-		elif text == 'Curva':
-			#docurva
-			pass
-		elif text == 'Clotoide':
-			#doclot
-			pass
+        length_edit = QtGui.QLineEdit('Digite o comprimento.', parent)
+
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(length, 1, 0)
+        grid.addWidget(length_edit, 1, 1)
+
+        self.setWindowTitle(u'Opções de Reta')
+
+	return grid
+
+    def make_curve(self, parent):
+        angle = QtGui.QLabel(u'Arco: ', parent)
+        radius = QtGui.QLabel('Raio: ', parent)
+
+        angle_edit = QtGui.QLineEdit('Digite o arco de curva.', parent)
+        radius_edit = QtGui.QLineEdit('Digite o raio da curva.', parent)
+
+        grid = QtGui.QGridLayout()
+        grid.setSpacing(10)
+
+        grid.addWidget(angle, 1, 0)
+        grid.addWidget(angle_edit, 1, 1)
+
+        grid.addWidget(radius, 2, 0)
+        grid.addWidget(radius_edit, 2, 1)
+
+        self.setWindowTitle(u'Opções de Curva')
+
+	return grid
+
+    def make_clothoid(self, parent):
+	pass
 
 
+class TrackCreator(QtGui.QGroupBox):
+    def __init__(self, parent):
+        super(TrackCreator, self).__init__('Escolha o tipo de trecho:', parent)
+        self.initUI(parent)
+
+    def initUI(self, parent):
+	#create main widgets and layouts
+	straight = QtGui.QPushButton('Reta', self)
+	curve = QtGui.QPushButton('Curva', self)
+	clothoid = QtGui.QPushButton(u'Clotóide', self)
+	self.main_layout = QtGui.QVBoxLayout()
+
+	#create buttons
+	self.main_layout.addWidget(straight)
+	self.main_layout.addWidget(curve)
+	self.main_layout.addWidget(clothoid)
+
+	parent.connect(straight, QtCore.SIGNAL('clicked()'), self.straight_dialog)
+	parent.connect(curve, QtCore.SIGNAL('clicked()'), self.curve_dialog)
+	parent.connect(clothoid, QtCore.SIGNAL('clicked()'), self.clothoid_dialog)
+
+	#make layout
+	self.main_layout.addWidget(straight)
+	self.main_layout.addWidget(curve)
+	self.main_layout.addWidget(clothoid)
+	self.setLayout(self.main_layout)
+
+    def straight_dialog(self):
+	dlg = GetOpts("Reta")
+	dlg.show()
+
+    def curve_dialog(self):	
+	pass
+
+    def clothoid_dialog(self):	
+	pass
+	
 
 class MainWindow(QtGui.QWidget):
   
@@ -59,12 +126,11 @@ class MainWindow(QtGui.QWidget):
 
 	track_creator = TrackCreator(self)
 
-
 	testBtn3 = QtGui.QPushButton('Test3', self)
 
 	splitter = QtGui.QSplitter(QtCore.Qt.Horizontal)	
 	splitter.addWidget(testBtn3)
-	splitter.addWidget(track_creator.container)
+	splitter.addWidget(track_creator)
 
 	vbox = QtGui.QVBoxLayout()
 	vbox.addWidget(splitter)
