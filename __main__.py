7f0cd03151dc036f05404ed6186fea004f44a385
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2011-10-06 09:00:34 -0300

    Deleted useless code on __main__

--- __main__.py
+++ __main__.py
@@ -3,26 +3,4 @@
 PROJECT_DIR = "/home/echobravo/Projects/YMSim"
 from sys import path
 path.append(PROJECT_DIR)
-from gallery.land_vehicles import *
-from utils import *
-
-
-car = Car(Condition(4.47, 11.84, Position(0,0), psi = sp.pi/2))
-
-car.move(0 , 30, car.acc_long_trac, car.acc_lat, dt = 0.1)
-
-print(len(car.conditions))
-2))
-
-car.move(0 , 30, car.acc_long_trac, car.acc_lat, dt = 0.1)
-
-print(len(car.conditions))
-1)
-
-print(len(car.conditions))
-ong_trac, car.acc_lat, dt = 0.1)
-
-print(len(car.conditions))
-1)
-
-print(len(car.conditions))
+from physics.physics import Car
