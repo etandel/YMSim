03dc4e0658dfc678d5d62ef9764fef8790f11274
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2011-10-06 08:54:15 -0300

    Transfered Car class to physics module.

--- physics/physics.py
+++ physics/physics.py
@@ -63,3 +63,17 @@ class Vehicle_Dynamics():
 
 		conds_f = Condition(conds_i.acc_max, conds_i.tau, Position(X,Y), psi, speed, omega, radius=conds_i.radius)
 		return conds_f
+
+
+class Car(Vehicle_Dynamics):
+	def __init__(self, conditions):
+		self.conditions = []
+		self.conditions.append(conditions)
+
+
+	def move(self, ti, tf, acc_long, acc_lat, dt=0.1):
+		tmax = (tf-ti)/dt
+		i = len(self.conditions)
+		while i <= tmax:
+			self.conditions.append(self.get_next_conditions(self.conditions[i-1], acc_long, acc_lat, dt))
+			i += 1
